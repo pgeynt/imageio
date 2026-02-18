b59c84c8d7274f4eef7dc66e8677fe3c9fee6893
Pgeynt <a.sakarya@elmacik.com>
2026-02-18 12:14:26 +0300

    index.php add ersdtcjkgvuyblh babyyyy

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * Root front controller: Apache DocumentRoot /var/www/html iken
+ * GET / bu dosyayi calistirir; uygulama public/index.php'de.
+ */
+require __DIR__ . '/public/index.php';
